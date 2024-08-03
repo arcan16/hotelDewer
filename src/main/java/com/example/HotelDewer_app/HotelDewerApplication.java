0337d01ba9ff44f7f04bc11ed73a88e04d8eb45b
arcan16 <sergiomar1@hotmail.es>
2024-08-03 13:10:39 -0600

    First commit

---
+++ src/main/java/com/example/HotelDewer_app/HotelDewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HotelDewer_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelDewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HotelDewer_app/HotelDewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HotelDewer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
